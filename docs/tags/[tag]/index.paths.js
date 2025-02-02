eb12a5c87551ff94ce5c7b16a3e0e8ce3b9bfec0
bqc0n <b4c0nchan@gmail.com>
2025-02-02 11:05:02 +0900

    remove unnecessary logging

--- docs/tags/[tag]/index.paths.js
+++ docs/tags/[tag]/index.paths.js
@@ -8,9 +8,7 @@ var files = globSync("docs/posts/**/*.md");
 files.forEach(file => {
     var data = fs.readFileSync(file, 'utf8');
     var found = data.match(/^tags:\s*\[(.+)]\s*$/m)
-    console.log(found)
     if (found) {
-        console.log(found[1])
         found[1].split(",")
             .map(tag => { return tag.replaceAll('"', '') })
             .forEach(tag => {
