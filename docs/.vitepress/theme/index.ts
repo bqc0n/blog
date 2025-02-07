969e10c55258cb49c5c21a3bc0d3719c1046141e
bqc0n <b4c0nchan@gmail.com>
2025-02-07 17:27:50 +0900

    git changelog, lightbox plugin

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import 'virtual:group-icons.css'
+import { NolebaseGitChangelogPlugin } from "@nolebase/vitepress-plugin-git-changelog/client"
+import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
+import Layout from "./Layout.vue";
 
-export default DefaultTheme
+export default {
+    ...DefaultTheme,
+    Layout,
+    enhanceApp({app}) {
+        app.use(NolebaseGitChangelogPlugin)
+    }
+}
